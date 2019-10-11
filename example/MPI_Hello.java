442974551aa127d8111ac34403dc4dd74c7858a0
a3y3 <sohamssd@gmail.com>
2019-10-10 20:05:04 -0400

    Add example

---
+++ example/MPI_Hello.java
@@ -0,0 +1,17 @@
+package edu.rit.cs;
+
+import mpi.MPI;
+import mpi.MPIException;
+
+public class MPI_Hello {
+    public static void main(String args[]) throws MPIException {
+        MPI.Init(args);
+
+        int rank = MPI.COMM_WORLD.getRank(), size = MPI.COMM_WORLD.getSize();
+
+        String hostname = MPI.getProcessorName();
+        System.out.println("From Java Program: Number of tasks= "+size+", My rank="+rank+", Running on "+hostname);
+
+        MPI.Finalize();
+    }
+}
